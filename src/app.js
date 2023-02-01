fe8b09a956c9b05098ba2972b8a835e52153c40e
bieehoangg <devbieehoangiogmail.com>
2023-02-01 10:51:51 +0700

    Express handlebars

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { title } from 'process';
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -11,14 +12,22 @@ const app = express();
 const port = 4000;
 
 app.use(morgan('combined'));
-app.engine('.hdb', engine({extname: '.hdb'}))
+app.engine('.hdb', engine(
+	{extname: '.hdb'}
+));
+
 app.set('view engine', '.hdb')
 app.set('views', path.join(__dirname,'resouces/views'))
-console.log(__dirname)
+
 app.get('/', (req,res) => {
-	res.render('home') 
+	res.render('home', {
+		title:"home",
+		}) 
+});
+app.get('/news', (req, res) => {
+	res.render('news')
 });
 
 app.listen(port,() => {
-	console.log(`Listening in ${port}`)
+	console.log(`Listening in http://localhost:${port}`)
 	})
