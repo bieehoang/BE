d7ce8fb15a2f7f122b76cc008d6ff4bd031f98bc
devsbac <bacsvn.dev@gmail.com>
2023-02-09 11:12:18 +0700

    src/app.js

--- src/app.js
+++ src/app.js
@@ -4,30 +4,32 @@ import morgan from 'morgan';
 
 import path from 'path';
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 4000;
+const port = 3000;
 
-app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname, 'public/')))
-app.engine('.hdb', engine(
-	{extname: '.hdb'}
-));
+// app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public/')));
+app.engine('.hdb', engine({ extname: '.hdb' }));
 
-app.set('view engine', '.hdb')
-app.set('views', path.join(__dirname,'resouces/views'))
+app.set('view engine', '.hdb');
+app.set('views', path.join(__dirname, 'resouces/views'));
 
-app.get('/', (req,res) => {
-	res.render('home', {
-		title:"Home",
-		});
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Home',
+    });
 });
 app.get('/news', (req, res) => {
-	res.render('news')
+    res.render('news');
+    
+	
+	
+	console.log(req.query);
 });
 
-app.listen(port,() => {
-	console.log(`Listening in http://localhost:${port}`)
-	})
+					app.listen(port, () => {
+    console.log(`Listening in http://localhost:${port}`);
+});
